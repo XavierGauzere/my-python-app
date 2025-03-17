f7874044953bacb1f05e69b2b77d17495c881628
XavierGauzere <xavier.gauzere@gmail.com>
2025-03-17 11:08:36 +0100

    Ajout du workflow GitHub Actions

--- main.py
+++ main.py
@@ -2,5 +2,8 @@
 def add(a, b):
     return a + b
 
+def essai(a):
+    return a * a
+
 if __name__ == "__main__":
     print("3 + 5 =", add(3, 5))
--- test_main.py
+++ test_main.py
@@ -1,6 +1,11 @@
 # test_main.py
-from main import add
+from main import add, essai
 
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
+
+def test_essai():
+    assert essai(2) == 4
+    assert essai(3) == 9
+    assert essai(0) == 0
